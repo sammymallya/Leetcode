a028c708f5bb90653d5c2c8838cca9b5d4ca6adf
sammymallya <144169439+sammymallya@users.noreply.github.com>
2025-04-12 18:14:09 +0530

    Sliding Window

---
+++ All_Problems/#643-Maximum_Average_Subarray_I.py
@@ -0,0 +1,22 @@
+#Tags: Easy, Sliding_Window
+#problem: 643-Maximum Average Subarray I, link =https://leetcode.com/problems/maximum-average-subarray-i
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: float
+        """
+        current_sum= sum(nums[:k])
+        max_sum = current_sum
+        for i in range(k,len(nums)):
+            current_sum += nums[i] - nums[i-k]
+            max_sum= max(max_sum, current_sum)
+        return float(max_sum) /k
+
+        
+
+
+
+
+        
